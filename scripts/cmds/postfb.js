bdbe723391126aaea72b58243ac55cf853643e4f
Asif-Mahmud-Joy <vivoa1s523@gmail.com>
2025-06-17 17:40:14 +0600

    Update postfb.js

--- scripts/cmds/postfb.js
+++ scripts/cmds/postfb.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 module.exports = {
   config: {
-    name: "postfb",
+    name: "postfbv2",
     version: "2.0",
     author: "✨ Mr.Smokey [Asif Mahmud] ✨",
     countDown: 5,
