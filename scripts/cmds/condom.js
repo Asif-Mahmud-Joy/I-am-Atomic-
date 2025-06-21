db5b82b6a58abe94bfdb5db114b03236d3f49f51
Asif-Mahmud-Joy <vivoa1s523@gmail.com>
2025-06-21 22:17:02 +0600

    Update condom.js

--- scripts/cmds/condom.js
+++ scripts/cmds/condom.js
@@ -1,60 +1,52 @@
 const axios = require('axios');
 const jimp = require("jimp");
-const fs = require("fs-extra");
-const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   config: {
     name: "condom",
     aliases: ["condom"],
-    version: "1.1",
-    author: "🎩 𝐌𝐫.𝐒𝐦𝐨𝐤𝐞𝐲 • 𝐀𝐬𝐢𝐟 𝐌𝐚𝐡𝐦𝐮𝐝 🌠",
+    version: "1.0",
+    author: "Asif",
     countDown: 5,
     role: 0,
-    shortDescription: "Banglish: Bondhuder nia moja koro with condom fail DP 😆",
-    longDescription: "Banglish: Tag kora bondhur face ekta condom fail image-e boshe debe moja korar jonno.",
+    shortdescription: "Make fun of your friends",
+    longDescription: "Make fun of your friends using crazy condom fails",
     category: "fun",
-    guide: "{pn} @mention"
+    guide: ""
   },
 
-  onStart: async function ({ message, event }) {
+  onStart: async function ({ message, event, args }) {
     const mention = Object.keys(event.mentions);
-
-    if (mention.length === 0) {
-      return message.reply("😑 Tag diye ekjon ke mention korte hobe!");
+    if (mention.length == 0) {
+      message.reply("You must select tag a person");
+      return;
     }
 
-    const uid = mention[0];
+    let one;
+    if (mention.length == 1) {
+      one = mention[0];
+    } else {
+      one = mention[0];
+    }
 
     try {
-      const outputPath = await createImage(uid);
+      const imagePath = await bal(one);
       await message.reply({
-        body: "😆 Ops! Condom Fail Face Fusion Ready!",
-        attachment: fs.createReadStream(outputPath)
+        body: "Ops Crazy Condom Fails😆",
+        attachment: fs.createReadStream(imagePath)
       });
-
-      // Auto delete file after send
-      fs.unlinkSync(outputPath);
-    } catch (err) {
-      console.error("❌ Error in condom command:", err);
-      return message.reply("❌ DP banate somossa hoise. Try again later.");
+    } catch (error) {
+      console.error("Error while running command:", error);
+      await message.reply("an error occurred");
     }
   }
 };
-
-async function createImage(uid) {
-  const avatarUrl = `https://graph.facebook.com/${uid}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`;
-
-  const [avatar, background] = await Promise.all([
-    jimp.read(avatarUrl),
-    jimp.read("https://i.imgur.com/cLEixM0.jpg") // condom meme background
-  ]);
-
-  background.resize(512, 512);
-  avatar.resize(263, 263);
-  background.composite(avatar, 256, 258);
-
-  const outputPath = path.join(__dirname, `condom_${uid}_${Date.now()}.png`);
-  await background.writeAsync(outputPath);
-  return outputPath;
+async function bal(one) {
+  const avatarone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+  const image = await jimp.read("https://i.imgur.com/cLEixM0.jpg");
+  image.resize(512, 512).composite(avatarone.resize(263, 263), 256, 258);
+  const imagePath = "condom.png";
+  await image.writeAsync(imagePath);
+  return imagePath;
 }
